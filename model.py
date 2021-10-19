74e7d29a3e32cc04385638be8a29878e9ef66665
chalonhutson <chalonhutson@gmail.com>
2021-10-19 15:34:15 -0600

    Edited model.py file to debug Heroku problems.

--- model.py
+++ model.py
@@ -147,7 +147,7 @@ class Meetings_notes(db.Model):
 
 def connect_to_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = environ["HEROKU_POSTGRESQL_JADE_URL"]
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
 
@@ -155,6 +155,6 @@ def connect_to_db(app):
 # Allows us to run code interactively and work with the database directly.
 if __name__ == "__main__":
     from server import app
-    db.create_all()
+    # db.create_all()
     connect_to_db(app)
     print("Connected to Easy CRM database.")
